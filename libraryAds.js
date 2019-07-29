12412362d6100746df8df13b1e5bb5609f799b84
Angel Hernandez Licona <angel.hernandez@televisatim.com>
2019-07-29 13:00:11 -0500

    CHANGE: Se agregaron los costructores de las clases

--- libraryAds.js
+++ libraryAds.js
@@ -2,9 +2,23 @@
  * * Libreria de Ads para cualquier sitio
  */
 
-console.log('hola')
-function hola () {
-    console.log(aqui );
+document.addEventListener('DOMContentLoaded', (e) => {
+    console.log('Todo cargo', e)
+
+    let ci = new gpt()
+    console.log('saludo', ci.getSaludo)
+    console.log(ci.saludo);
+});
+
+
+class gpt {
+    constructor() {
+        this.saludo = 'hola';
+        console.log('constructor gpt');
+    }
+
+    get getSaludo() {
+        return this.saludo
+    }
+
 }
-
-
